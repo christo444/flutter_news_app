5864bd3cf90317ab6794c92a97e25e6f961d1368
Christo Berly <christoberly2882@gmail.com>
2025-07-15 21:15:24 +0530

    Update api.dart

--- lib/API/api.dart
+++ lib/API/api.dart
@@ -6,7 +6,6 @@ class NewsApiService {
   static const String _baseUrl = 'https://newsapi.org/v2/top-headlines';
   static const String _apiKey = 'fcdb78e08f43409ea06f0df045f39bc9';
 
-  /// Fetch top headlines by country and category
   Future<NewsModel> fetchNews({String country = 'us'}) async {
     final url = Uri.parse('$_baseUrl?country=$country&category=technology');
 
@@ -18,7 +17,7 @@ class NewsApiService {
     );
 
     if (response.statusCode == 200) {
-      print("RESPONSE JSON: ${response.body}"); // for debugging
+      print("RESPONSE JSON: ${response.body}");
       final jsonData = jsonDecode(response.body) as Map<String, dynamic>;
       return NewsModel.fromJson(jsonData);
     } else {
